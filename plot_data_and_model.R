f114f26d33d1c71e44f9b4fe54139e34536fc86a
oliverdonald <oliver.donald@jesus.ox.ac.uk>
2024-10-30 17:05:38 +0000

    Commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,27 +1,24 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
-  
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
-  
   return(N)
-  
 }
 
-N0 <- ??? #
+N0 <- 879 #
   
-r <- ??? #
+r <- 0.0100086 #
   
-K <- ??? #
+K <- 6e+10 #
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
-  geom_point()
+  geom_point() +
 
-  #scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10')
 
 
